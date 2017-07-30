398d4afb73845c490dd392d7337013ceabde5f86
ellen <ellenlouisehaggan@gmail.com>
2017-07-30 15:38:46 +0100

    name modifications

--- blog/views.py
+++ blog/views.py
@@ -1,42 +1,42 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post 
+from .models import Standard 
 from .forms import PostForm
 
 # Create your views here.
 
-def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {'posts': posts})
+def full_list(request):
+	pageslist = Standard.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/standard_list.html', {'pages': pages})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+def standard_detail(request, pk):
+    page = get_object_or_404(Standard, pk=pk)
+    return render(request, 'blog/standard_detail.html', {'page': page})
 
-def post_new(request):
+def standard_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
-			post = form.save(commit=False)
-			post.author = request.user
-			post.published_date = timezone.now()
-			post.save()
-			return redirect('post_detail', pk=post.pk)
+			page = form.save(commit=False)
+			page.author = request.user
+			page.published_date = timezone.now()
+			page.save()
+			return redirect('standard_detail', pk=post.pk)
 	else:
 		form = PostForm()
-	return render(request, 'blog/post_edit.html', {'form': form})
+	return render(request, 'blog/standard_edit.html', {'form': form})
 	
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def standard_edit(request, pk):
+    page = get_object_or_404(Standard, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+            page = form.save(commit=False)
+            page.author = request.user
+            page.published_date = timezone.now()
+            page.save()
+            return redirect('standard_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})	
+    return render(request, 'blog/standard_edit.html', {'form': form})	
 	
