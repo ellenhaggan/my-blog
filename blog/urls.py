8a136d9b78700fabb5ab6ad8e13b7a0eacc35c68
ellen <ellenlouisehaggan@gmail.com>
2017-07-30 16:02:21 +0100

    further errors

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.post_list, name='post_list'),
-	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-	url(r'^post/new/$', views.post_new, name='post_new'),
-	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+	url(r'^$', views.full_list, name='full_list'),
+	url(r'^page/(?P<pk>\d+)/$', views.standard_detail, name='page_detail'),
+	url(r'^page/new/$', views.standard_new, name='page_new'),
+	url(r'^page/(?P<pk>\d+)/edit/$', views.standard_edit, name='page_edit'),
 	]
